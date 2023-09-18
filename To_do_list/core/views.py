9df3a4e02d91c1c5130e1b4e49aac2a40506e0a3
sk soyab akhtar <sksoyabakhtar3@gmail.com>
2023-09-18 10:16:12 +0530

    Second Commit

--- To_do_list/core/views.py
+++ To_do_list/core/views.py
@@ -3,7 +3,23 @@ from .models import Task
 
 def task_list(request):
     tasks = Task.objects.all()
+
+    if request.method == 'POST':
+        task_id = request.POST.get('task_id')
+        action = request.POST.get('action')
+
+        if action == 'mark_complete':
+            task = Task.objects.get(id=task_id)
+            task.completed = True
+            task.save()
+        elif action == 'mark_incomplete':
+            task = Task.objects.get(id=task_id)
+            task.completed = False
+            task.save()
+
     return render(request, 'task_list.html', {'tasks': tasks})
+
+
 
 def add_task(request):
     if request.method == 'POST':
@@ -26,7 +42,7 @@ def update_task(request, task_id):
     if request.method == 'POST':
         title = request.POST.get('title')
         description = request.POST.get('description')
-        completed = request.POST.get('completed') 
+        completed = request.POST.get('completed')
 
         completed = completed == 'on'
 
@@ -42,3 +58,6 @@ def delete_task(request, task_id):
     task = Task.objects.get(id=task_id)
     task.delete()
     return redirect('task_list')
+
+def status_task(request, task_id):
+    return render(request, 'status_task.html')
